56cf64f7e3baa59315236d9eccbb2f6b72ca9a29
Lars Vogel <Lars.Vogel@vogella.com>
2017-03-03 14:44:52 +0100

    Adds how to remove and add components dynamically to a layout at runtime.

--- SWT/res/positioning/SWTLayoutPositioning.java
+++ SWT/res/positioning/SWTLayoutPositioning.java
@@ -1,18 +1,22 @@
 package com.vogella.swt.widgets.layout;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
-public class SWTLayoutPositioning {
+public class SWTLayoutPositionTracker {
 
 	static String newLine = System.getProperty("line.separator");
+	private static Label positiongLabel;
+	private static Shell shell;
 	public static void main(String[] args) {
 		Display display = new Display();
-		Shell shell = new Shell(display);
+		shell = new Shell(display);
 
-		Label positiongLabel = new Label(shell, SWT.BORDER);
+		positiongLabel = new Label(shell, SWT.BORDER);
 		
 		int x= 60;
 		int y=20;
@@ -20,22 +24,30 @@ public class SWTLayoutPositioning {
 		int height=200;
 	
 		positiongLabel.setBounds(x, y, width, height);
-
-		// approx. shell toolbar size
 		int toolbarSize = 30;
-		// initial position
+
 		shell.setBounds(200, 400, width+2*x , height + 2*y +toolbarSize);
 		shell.open();
 		
 		
-		String s = "Bounds for Label: " + positiongLabel.getBounds() + newLine;
-		s+= "Bounds for Shell: " + shell.getBounds();
-		positiongLabel.setText(s);
+		
+		shell.addMouseMoveListener(e -> showSize(e));
+		positiongLabel.addMouseMoveListener(e -> showSize(e));
 		
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch())
 				display.sleep();
 		}
 		display.dispose();
+	}
+	
+	public static void showSize(MouseEvent e) {
+		int x = e.x;
+		int y = e.y;
+		String s = "Bounds for Label: " + positiongLabel.getBounds() + newLine;
+		s+= "Bounds for Shell: " + shell.getBounds()  + newLine;
+		s+= "Mouse pointer: " + x + " " + y;
+		positiongLabel.setText(s);
+		
 	}
 }
